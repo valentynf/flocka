51b48906dcc3fee7f829445eaee8264f52eeeb0e
valentynf <valentynf@wix.com>
2023-10-09 20:39:51 +0300

    design adjustments for CollapsibleList

--- src/components/CollapsibleListItem/CollapsibleListItem.tsx
+++ src/components/CollapsibleListItem/CollapsibleListItem.tsx
@@ -2,6 +2,8 @@ type CollapsibleListItemPropsType = {
   name: string;
 };
 
+//update item to have an icon and name for now - icon is either a # or a lock
+
 function CollapsibleListItem({ name }: CollapsibleListItemPropsType) {
   return <li>{name}</li>;
 }
