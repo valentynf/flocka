e7ecd6a7c8b499d9546a83efb525ddfe37e8af2b
valentynf <valentynf@wix.com>
2023-10-16 20:44:29 +0300

    reverting changes made to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import HomeView from './components/HomeView/HomeView';
 import styles from './App.module.css';
-import AppSidebar from './components/AppSidebar/AppSidebar';
-
-// function App() {
-//   return (
-//     <div className={styles['main-view']}>
-//       <div className={styles['main-topbar']}></div>
-//       <div className={styles['main-container']}>
-//         <div className={styles['main-sidebar']}></div>
-//         <div className={styles['main-content']}>
-//           <HomeView />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
 
 function App() {
-  return <AppSidebar />;
+  return (
+    <div className={styles['main-view']}>
+      <div className={styles['main-topbar']}></div>
+      <div className={styles['main-container']}>
+        <div className={styles['main-sidebar']}></div>
+        <div className={styles['main-content']}>
+          <HomeView />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
