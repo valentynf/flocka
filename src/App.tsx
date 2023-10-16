910f1a818eff1259be6699f9524f6784c9b41128
valentynf <valentynf@wix.com>
2023-10-16 21:25:52 +0300

    added sidebar to the main screen, no design just yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import HomeView from './components/HomeView/HomeView';
 import styles from './App.module.css';
+import AppSidebar from './components/AppSidebar/AppSidebar';
 
 function App() {
   return (
     <div className={styles['main-view']}>
       <div className={styles['main-topbar']}></div>
       <div className={styles['main-container']}>
-        <div className={styles['main-sidebar']}></div>
+        <div className={styles['main-sidebar']}>
+          <AppSidebar />
+        </div>
         <div className={styles['main-content']}>
           <HomeView />
         </div>
