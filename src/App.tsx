95073d8b94f8b588862e2248112ef9b7541d446a
valentynf <valentynf@wix.com>
2023-10-17 12:19:15 +0300

    removed collapsible panel because it caused layout issues, added AppTopbar to the main view

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import HomeView from './components/HomeView/HomeView';
 import styles from './App.module.css';
 import AppSidebar from './components/AppSidebar/AppSidebar';
+import AppTopBar from './components/AppTopbar/AppTopbar';
 
 function App() {
   return (
     <div className={styles['main-view']}>
-      <div className={styles['main-topbar']}></div>
+      <div className={styles['main-topbar']}>
+        <AppTopBar />
+      </div>
       <div className={styles['main-container']}>
         <div className={styles['main-sidebar']}>
           <AppSidebar />
