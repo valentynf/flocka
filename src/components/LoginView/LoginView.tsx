873cbaf406326d19362e741b0186af5675b9a389
valentynf <valentynf@wix.com>
2023-10-21 19:30:36 +0300

    switched to a custom button to avoid dealing with jwt

--- src/components/LoginView/LoginView.tsx
+++ src/components/LoginView/LoginView.tsx
@@ -1,14 +1,44 @@
-import { GoogleLogin } from '@react-oauth/google';
+import { TokenResponse, useGoogleLogin } from '@react-oauth/google';
 import styles from './LoginView.module.css';
+import { useEffect, useState } from 'react';
 
 function LoginView() {
+  const [user, setUser] = useState<TokenResponse>({} as TokenResponse);
+
+  const login = useGoogleLogin({
+    onSuccess: (tokenResponse) => setUser(tokenResponse),
+  });
+
+  useEffect(() => {
+    if (user.access_token) {
+      fetch(
+        `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${user.access_token}`,
+            Accept: 'application/json',
+          },
+        }
+      )
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(
+          (data) => {
+            console.log(data);
+          },
+          [user]
+        );
+    }
+  });
+
   return (
     <div className={styles['login-container']}>
-      <GoogleLogin
-        theme="filled_black"
-        onSuccess={(credRes) => console.log(credRes)}
-        onError={() => console.log('login failed')}
-      />
+      <button onClick={() => login()}> LOGIN VIA GOGLE</button>
     </div>
   );
 }
