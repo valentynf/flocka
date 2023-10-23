5d4248558f8c99f44c31a9de457c3cd3d96c8238
valentynf <valentynf@wix.com>
2023-10-23 20:25:26 +0300

    added routing, installed packaged for supabase

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,56 @@ import AppSidebar from './components/AppSidebar/AppSidebar';
 import AppTopBar from './components/AppTopbar/AppTopbar';
 import LoginView from './components/LoginView/LoginView';
 import useUser from './hooks/useUser';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 
 function App() {
   const { user_data } = useUser();
 
   return (
-    <div className={styles['main-view']}>
-      {Object.keys(user_data).length === 0 ? (
-        <LoginView />
-      ) : (
-        <div>
-          <div className={styles['main-topbar']}>
-            <AppTopBar />
-          </div>
-          <div className={styles['main-container']}>
-            <div className={styles['main-sidebar']}>
-              <AppSidebar />
-            </div>
-            <div className={styles['main-content']}>
-              <HomeView />
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
+    <Router>
+      <div className={styles['main-view']}>
+        <Routes>
+          <Route
+            path="/login"
+            element={
+              Object.keys(user_data).length === 0 ? (
+                <LoginView />
+              ) : (
+                <Navigate to="/app" />
+              )
+            }
+          />
+          <Route
+            path="/app"
+            element={
+              Object.keys(user_data).length === 0 ? (
+                <Navigate to="/login" />
+              ) : (
+                <div>
+                  <div className={styles['main-topbar']}>
+                    <AppTopBar />
+                  </div>
+                  <div className={styles['main-container']}>
+                    <div className={styles['main-sidebar']}>
+                      <AppSidebar />
+                    </div>
+                    <div className={styles['main-content']}>
+                      <HomeView />
+                    </div>
+                  </div>
+                </div>
+              )
+            }
+          />
+          <Route path="/*" element={<Navigate to="/app" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
