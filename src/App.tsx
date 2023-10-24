278764d6c5432edd49989455adee5a42332ad180
valentynf <valentynf@wix.com>
2023-10-24 21:34:05 +0300

    moved applayout to a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import HomeView from './components/HomeView/HomeView';
-import styles from './App.module.css';
-import AppSidebar from './components/AppSidebar/AppSidebar';
-import AppTopBar from './components/AppTopbar/AppTopbar';
+import AppLayout from './components/AppLayout/AppLayout';
 import LoginView from './components/LoginView/LoginView';
 import useUser from './hooks/useUser';
 import {
@@ -16,37 +13,17 @@ function App() {
 
   return (
     <Router>
-      <div className={styles['main-view']}>
-        <Routes>
-          <Route
-            path="/login"
-            element={session === null ? <LoginView /> : <Navigate to="/app" />}
-          />
-          <Route
-            path="/app"
-            element={
-              session === null ? (
-                <Navigate to="/login" />
-              ) : (
-                <div>
-                  <div className={styles['main-topbar']}>
-                    <AppTopBar />
-                  </div>
-                  <div className={styles['main-container']}>
-                    <div className={styles['main-sidebar']}>
-                      <AppSidebar />
-                    </div>
-                    <div className={styles['main-content']}>
-                      <HomeView />
-                    </div>
-                  </div>
-                </div>
-              )
-            }
-          />
-          <Route path="/*" element={<Navigate to="/app" />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route
+          path="/login"
+          element={session === null ? <LoginView /> : <Navigate to="/app" />}
+        />
+        <Route
+          path="/app"
+          element={session === null ? <Navigate to="/login" /> : <AppLayout />}
+        />
+        <Route path="/*" element={<Navigate to="/app" />} />
+      </Routes>
     </Router>
   );
 }
