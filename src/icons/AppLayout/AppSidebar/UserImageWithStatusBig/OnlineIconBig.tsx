62502767e37759fc165bf598a35c4b9f8d85565c
valentynf <valentynf@wix.com>
2023-11-01 10:24:05 +0200

    minor css adjustments to the avatar icon in appsidebar

--- src/icons/AppLayout/AppSidebar/UserImageWithStatusBig/OnlineIconBig.tsx
+++ src/icons/AppLayout/AppSidebar/UserImageWithStatusBig/OnlineIconBig.tsx
@@ -1,7 +1,7 @@
 function OnlineIconBig() {
   return (
     <svg viewBox="0 0 16 16" fill="#349169" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="8" cy="8" r="7.277" stroke="#202020" strokeWidth="3" />
+      <circle cx="8" cy="8" r="6.277" stroke="#202020" strokeWidth="2" />
     </svg>
   );
 }
