88e86bfb502820634bff3a841776e817b97f6c51
valentynf <valentynf@wix.com>
2023-11-08 22:05:12 +0200

    added some design for the register view

--- src/components/RegisterView/RegisterView.tsx
+++ src/components/RegisterView/RegisterView.tsx
@@ -1,7 +1,40 @@
 import styles from './RegisterView.module.css';
 
 function RegisterView() {
-  return <div className={styles['register']}>Register</div>;
+  return (
+    <div className={styles['register-view']}>
+      <div className={styles['form-container']}>
+        <h2 className={styles['heading']}>Register New User</h2>
+
+        <form>
+          <div className={styles['input-field']}>
+            <label>Email</label>
+            <input
+              className={styles['disabled']}
+              type="text"
+              value="user@email.com"
+              readOnly
+            />
+            <p className={styles['email-description']}>
+              Email you've used to login via Google, not possible to change
+            </p>
+          </div>
+          <div className={styles['input-field']}>
+            <label>Display Name</label>
+            <input type="text" />
+          </div>
+          <div className={styles['upload-field']}>
+            <label>Upload Image</label>
+            <input type="file" accept="image/*" />
+          </div>
+          <div className="form-field">
+            <button type="submit">Register</button>
+            <button className="logout-button">Logout</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default RegisterView;
