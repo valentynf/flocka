3b721d1ab5883ce458ef261d022f4f7f4e847a7b
valentynf <valentynf@wix.com>
2023-11-18 16:01:05 +0200

    more participantsButton design

--- src/components/AppLayout/HomeView/ConvoView/ConvoHeader/ParticipantsButton/ParticipantsButton.tsx
+++ src/components/AppLayout/HomeView/ConvoView/ConvoHeader/ParticipantsButton/ParticipantsButton.tsx
@@ -1,16 +1,37 @@
 import { useSelector } from 'react-redux';
 import styles from './ParticipantsButton.module.css';
 import { RootState } from '../../../../../../types/appTypes';
+import { getThreeParticipants } from '../../../../../../utils/helper';
 
 function ParticipantsButton() {
   const convoData = useSelector((state: RootState) => state.home.current_convo);
+  const usersData = useSelector(
+    (state: RootState) => state.app_data.users_data
+  );
 
-  const participants = convoData.channel.participants;
+  const convoParticipants = convoData.channel.participants;
+  const participantsToShow = getThreeParticipants(convoParticipants);
 
   return (
     <button className={styles['participants-button']}>
-      <div className={styles['three-photos']}></div>
-      <span>{participants.length}</span>
+      <div className={styles['three-photos']}>
+        {participantsToShow.map((participantId, index) => {
+          const imageSource = usersData[participantId]
+            ? usersData[participantId].avatar_src
+            : 'src/assets/images/user-image.jpeg';
+
+          return (
+            <img
+              className={`${styles['participant-photo']} ${
+                index === 1 ? styles['second-photo'] : ''
+              } ${index === 2 ? styles['third-photo'] : ''}`}
+              src={imageSource}
+              key={index}
+            ></img>
+          );
+        })}
+      </div>
+      <span>{convoParticipants.length}</span>
     </button>
   );
 }
