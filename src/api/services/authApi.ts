f6ba38f4e179ccc80434b907edcf2efbbcd41817
valentynf <valentynf@wix.com>
2023-12-11 19:28:44 +0200

    some local dev adjustments to the login

--- src/api/services/authApi.ts
+++ src/api/services/authApi.ts
@@ -1,7 +1,13 @@
 import supabase from '../supabase';
 
 export const authLogin = async () =>
-  supabase.auth.signInWithOAuth({ provider: 'google' });
+  supabase.auth.signInWithOAuth({
+    provider: 'google',
+    //temporary solution for local dev process
+    options: {
+      redirectTo: 'http://localhost:5173/app',
+    },
+  });
 
 export const authSignOut = async () => supabase.auth.signOut();
 
